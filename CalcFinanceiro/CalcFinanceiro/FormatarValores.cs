2c155241449745d0362c62b84e1ed94631f611e7
Pedro Vasconcellos <pedro.vasconcellos@bndv.com.br>
2017-07-18 11:07:24 +0000

    2017-07-18 Commit Inicial

---
+++ CalcFinanceiro/CalcFinanceiro/FormatarValores.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// ============================================= 
+// Author:    Pedro Henrique Vasconcellos 
+// Create date: 26/04/2017 
+// Description:  Devolve uma String com a formatação
+// =============================================
+
+namespace CalcFinanceiro
+{
+    public class FormatarValores
+    {
+        public string PercentFormat(int CasasDecimais)
+        {
+            string PercentFormat;
+            switch (CasasDecimais)
+            {
+                case 1:
+                    PercentFormat = "#0.0";
+                    break;
+                case 2:
+                    PercentFormat = "#0.00";
+                    break;
+                case 3:
+                    PercentFormat = "#0.000";
+                    break;
+                case 4:
+                    PercentFormat = "#0.0000";
+                    break;
+                default:
+                    PercentFormat = "#0.00";
+                    break;
+            }
+
+            return PercentFormat;
+        }
+
+    }
+}
+
---
+++ CalcFinanceiro/CalcFinanceiro/TaxaInternaRetorno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+// ============================================= 
+// Author:    Pedro Henrique Vasconcellos 
+// Create date: 26/04/2017 
+// Description:  Cálculos Financeiro
+// =============================================
+
+namespace CalcFinanceiro
+{
+    public class TaxaInternaRetorno
+    {
+        public int Parcelas { get; set; }
+        public float ValorParcela { get; set; }
+        public float ValorFinanciado { get; set; }
+        public int CasasDecimais { get; set; }
+
+        // Taxa Interna De Retorno TIR
+        public double CalcularTaxa()
+        {
+            double[] Valores = new double[Parcelas + 1];
+            Valores[0] = ValorFinanciado * (-1);
+            for (int i = 1; i < Parcelas + 1; i++)
+            {
+                Valores[i] = ValorParcela;
+            }
+            //TIR - Utilizando taxa de advinhação de 10%
+            double Guess = 0.1;
+            double CalcRetRate = Financial.IRR(ref Valores, Guess) * 100;
+
+            FormatarValores FormatarValores = new FormatarValores();
+            return Double.Parse(Strings.Format(CalcRetRate, FormatarValores.PercentFormat(CasasDecimais)));
+        }
+    }
+}
